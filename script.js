26801914f8ea7634c6383ef0b5ffae931e78427e
Ivanna <ivanaiva063@gmail.com>
2023-03-30 16:53:50 +0300

    result view

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
- 
-const myDefaultId = document.getElementById('defaultId')
-console.log(myDefaultId)
-// myDefaultId.innerHTML = 'Hello Javascript!'
+const myDefaultId = document.getElementById("defaultId");
+//console.log(myDefaultId)
+//myDefaultId.innerHTML = 'Hello Javascript!'
 
-
-const myFunction = ()=>{
-    const message = prompt('Hello World!')
-
-    const createdDiv = document.createElement('div')
-    createdDiv.textContent = message
-    myDefaultId.appendChild(createdDiv)
+const myFunction = () => {
+  const message = prompt("Введіть повідомлення!");
+  console.log(message);
+  if (message.length < 5) {
+    prompt("Повідомлення закоротке!");
+  }
+  if (message.length >= 5 && message.length <= 20) {
+    const createdDiv = document.createElement("div");
+    createdDiv.textContent = message;
+    myDefaultId.appendChild(createdDiv);
     
-    // if(myDefaultId.textContent === 'Hello World!' )
-        // myDefaultId.textContent = message
-    // else myDefaultId.textContent = message
+  }
+  if (message.length > 20) {
+    const createdDiv = document.createElement("div");
+    createdDiv.textContent = message;
+    myDefaultId.appendChild(createdDiv);
+  }
 
-}
-
+  // if(myDefaultId.textContent === 'Hello World!' )
+  // myDefaultId.textContent = message
+  // else myDefaultId.textContent = message
+};
