335ff64b1521f7d345ccbf4078be7dfbadbea202
Ivanna <ivanaiva063@gmail.com>
2023-04-10 19:22:41 +0300

    pokemon

---
+++ pokemon.js
@@ -0,0 +1,55 @@
+let getPokemonButton = document.querySelector(".buttonPokemon");
+let cardPokemon = document.querySelector(".card__pokemon");
+
+getPokemonButton.addEventListener("click", (e) => fetchKantoPokemon(e));
+
+function fetchKantoPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+    .then((response) => response.json())
+    .then((allpokemon) => {
+      console.log(allpokemon);
+      allpokemon.results.forEach((pokemon) => {
+        fetchPokemonData(pokemon);
+        console.log(pokemon);
+      });
+    });
+}
+
+function fetchPokemonData(pokemon) {
+  let url = pokemon.url;
+  fetch(url)
+    .then((response) => response.json())
+    .then((pokemonData) => {
+      console.log(pokemonData);
+      renderPokemon(pokemonData);
+    });
+}
+
+function renderPokemon(pokemonData) {
+  let allPokemonContainer = document.querySelector("#poke-container");
+  let pokemonContainer = document.createElement("div");
+
+  pokemonContainer.className = "cardPokemon";
+  createPokeImage(pokemonData.id, pokemonData.name, pokemonContainer);
+
+  let pokeName = document.createElement("h4");
+  pokeName.innerText = pokemonData.name;
+
+  let pokeNumber = document.createElement("p");
+  pokeNumber.innerText = `${pokemonData.id}`;
+
+  pokemonContainer.append(pokeName, pokeNumber);
+  allPokemonContainer.appendChild(pokemonContainer);
+}
+
+function createPokeImage(pokemonID, pokemonName, containerDiv) {
+  let pokeImgContainer = document.createElement("div");
+  pokeImgContainer.className = "imageContainer";
+
+  let pokeImage = document.createElement("img");
+  pokeImage.src = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonID}.svg`;
+  pokeImage.alt = `${pokemonName}`;
+
+  pokeImgContainer.append(pokeImage);
+  containerDiv.append(pokeImgContainer);
+}
